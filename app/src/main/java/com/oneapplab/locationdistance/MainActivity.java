e17ab61246edb72f1c51118469e55704bb87b830
mdhaider <nehal.hdr@gmail.com>
2017-03-26 21:08:14 +0530

    Distance between two hardcoded latlng

---
+++ app/src/main/java/com/oneapplab/locationdistance/MainActivity.java
@@ -0,0 +1,40 @@
+package com.oneapplab.locationdistance;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.SphericalUtil;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv= (TextView) findViewById(R.id.tv);
+        showDistance();
+
+
+    }
+    private void showDistance() {
+        LatLng fromPosition = new LatLng(23.4555453556, 11.145315551);
+        LatLng toPosition = new LatLng(12.1115145311, 99.333455333);
+        double distance = SphericalUtil.computeDistanceBetween(fromPosition,toPosition);
+        tv.setText("The distance between tho latlng is" + formatNumber(distance));
+    }
+    private String formatNumber(double distance) {
+        String unit = "m";
+        if (distance < 1) {
+            distance *= 1000;
+            unit = "mm";
+        } else if (distance > 1000) {
+            distance /= 1000;
+            unit = "km";
+        }
+
+        return String.format("%4.3f%s", distance, unit);
+    }
+}
